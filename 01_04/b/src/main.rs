2e5d74818d04dbd01b14e9ea3b3fc472ca806552
Daniel Vorhauer <daniel@hexerei.net>
2023-01-09 17:31:35 +0000

    optimized

--- 01_04/b/src/main.rs
+++ 01_04/b/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{self, BufRead, BufReader};
 
 fn main() {
     let file_name = "file_with_lines";
     let mut file = File::open(file_name).expect("Could not open file!");
-    let mut content = String::new();
-    let _ = file.read_to_string(&mut content);
-    for line in content.lines() {
-        println!("{}", line);
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+      println!("{}", line.unwrap());
     }
 }
