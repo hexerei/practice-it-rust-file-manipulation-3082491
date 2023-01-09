005f0885bb70eb334bcf582ca0f1733b637bd2de
Daniel Vorhauer <daniel@hexerei.net>
2023-01-09 17:41:17 +0000

    solved

--- 01_06/b/src/main.rs
+++ 01_06/b/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    let wanted_string = "a";
+    let wanted_string = "th";
+    let file_path = "file_with_lines";
+    let file: File = File::open(file_path).unwrap();
+    let reader = io::BufReader::new(file);
+    for line in reader.lines() {
+        let line = String::from(line.unwrap());
+        if line.contains(wanted_string) {
+            println!("{}", line);
+        }
+    }
 }
