2cb27389cfb360c999459ce37803f58f7d55b7d5
Daniel Vorhauer <daniel@hexerei.net>
2023-01-09 17:23:51 +0000

    solved

--- 01_04/b/src/main.rs
+++ 01_04/b/src/main.rs
@@ -1,3 +1,12 @@
 use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {}
+fn main() {
+    let file_name = "file_with_lines";
+    let mut file = File::open(file_name).expect("Could not open file!");
+    let mut content = String::new();
+    let _ = file.read_to_string(&mut content);
+    for line in content.lines() {
+        println!("{}", line);
+    }
+}
