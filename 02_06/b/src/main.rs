e5bde4c70187e647d69f073945ae91e1692556e2
Daniel Vorhauer <daniel@hexerei.net>
2023-01-10 16:43:22 +0100

    better error handling

--- 02_06/b/src/main.rs
+++ 02_06/b/src/main.rs
@@ -2,7 +2,8 @@ use std::fs;
 use std::io::Error;
 
 fn read_file(file_path: &str) -> Result<String, Error> {
-    fs::read_to_string(file_path)
+    let content = fs::read_to_string(file_path)?;
+    Ok(content)
 }
 
 fn main() {
