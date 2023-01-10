3510545df2ea5c28de436cedab7b9094e1ea6302
Daniel Vorhauer <daniel@hexerei.net>
2023-01-10 17:23:30 +0100

    solved

--- 03_11/b/src/main.rs
+++ 03_11/b/src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
     let file_path = "file_with_lines";
 
-    // Implement the read_file function
-    //let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
+    let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
+    println!("{:?}", lines);
+}
 
-    //println!("{:?}", lines);
+fn read_file(path: &str) -> Result<Vec<Vec<String>>, std::io::Error> {
+    let contents = std::fs::read_to_string(path)?;
+    Ok(
+        contents
+            .lines()
+            .map(|line|
+                line.split_whitespace()
+                    .map(|word| word.to_string())
+                    .collect::<Vec<String>>()
+            ).collect()
+        )
 }
