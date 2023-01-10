e4df2a09ac44f9120624c1824558bbe110c9f047
Daniel Vorhauer <daniel@hexerei.net>
2023-01-10 17:08:18 +0100

    solved

--- 03_02/b/src/main.rs
+++ 03_02/b/src/main.rs
@@ -1,8 +1,10 @@
+fn read_file(path: &str) -> Result<Vec<String>, std::io::Error> {
+    let content = std::fs::read_to_string(path)?;
+    Ok(content.lines().map(|line| line.to_string()).collect())
+}
 fn main() {
     let file_path = "file_with_lines";
 
-    // Implement the read_file function
-    //let lines: Vec<String> = read_file(&file_path).expect(&format!("Error reading file <{}>", &file_path));
-
-    //println!("{:#?}", lines);
+    let lines: Vec<String> = read_file(&file_path).expect(&format!("Error reading file <{}>", &file_path));
+    println!("{:#?}", lines);
 }
--- 03_05/b/src/main.rs
+++ 03_05/b/src/main.rs
@@ -1,8 +1,18 @@
+#[derive(Debug)]
+enum Error {
+    Unknown,
+    EmptyString,
+}
+fn get_words(content: &str) -> Result<Vec<String>, Error> {
+    if content.is_empty() {
+        return Err(Error::EmptyString);
+    }
+    Ok(content.split_whitespace().map(|word| word.to_string()).collect())
+
+}
 fn main() {
     let contents = String::from("This is the first line\nThe second line is a little longer\nLine 3 is short\nThe 4th line is the first non-prime\nThe 5th line has the starting five");
 
-    // Implement the get_words function
-    //let words = get_words(&contents);
-
-    //println!("{:?}", words);
+    let words = get_words(&contents);
+    println!("{:?}", words);
 }
